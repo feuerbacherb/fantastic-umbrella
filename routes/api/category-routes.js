644d58975f192e9530fd12a2b0e89ba1639d6614
Brent F <feuerbacherb@gmail.com>
2020-06-14 14:30:24 -0500

    correct minor issue with findOne category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -22,7 +22,11 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne({
+  Category.findOne(
+    {
+    where: {
+      id: req.params.id
+    },
     include: {
       model: Product,
       attributes: ['product_name']
